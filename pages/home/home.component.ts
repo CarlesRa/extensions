2d427ecf5ece1b4c8ab0413cebc41e5a8cd7c88b
CarlesRa <jcramos8282@gmail.com>
2021-09-26 23:47:31 +0200

    add gy-table example

--- pages/home/home.component.ts
+++ pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GyTablePropertyListModel} from "../../../ui-library/models/gy.table.property.list.model";
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  dataSource: any[] = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ];
+  tableData: GyTablePropertyListModel = {
+    modelAttributeName: [
+      'position', 'name', 'weight', 'symbol'
+    ],
+    displayedColumns: [
+      'Posición', 'Nombre', 'Peso', 'Símbolo'
+    ]
+  };
+
+
   constructor() { }
 
   ngOnInit(): void {}
+
+  printLog(type: string) {
+    console.log(type);
+  }
 
 }
